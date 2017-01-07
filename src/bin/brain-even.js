05dc33417f09302c41c6387df34f98f0b9f459f0
rende11 <artamonovn@gmail.com>
2017-01-07 09:08:52 +0300

    -add isEven game

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -2,6 +2,50 @@
 // @flow
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!\n');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
+const isEven = num => num % 2 === 0 ? 'yes' : 'no';
+
+const getRandomInt = (min, max) => 
+	Math.floor(Math.random() * (max - min)) + min;
+
+const getMyRandom = () => getRandomInt(0, 100);
+
+const showQuestion = (number) => {
+	console.log(`Question: ${number}`);
+};
+
+const getAnswer = () => 
+	readlineSync.question('Your answer: ');
+
+
+const play = (name) => {
+	const number = getMyRandom();
+	showQuestion(number);
+	const userChoise = getAnswer();
+	const correctChoise = isEven(number);
+	return correctChoise === userChoise ? 'Correct!' : `${userChoise} is wrong answer ;(. Correct answer was ${correctChoise}.`;
+};
+
+
+
+const playFullGame = maxAttempts => {
+	console.log('Welcome to the Brain Games!');
+	console.log('Answer "yes" if number odd otherwise answer "no".\n');
+	const name = readlineSync.question('May I have your name? ');
+	console.log(`Hello, ${name}!\n`);
+	
+	const iter = (acc) => {
+		if (acc  === maxAttempts) {
+			return `Congratulations, ${name}!`;
+		}
+		const result = play(name);
+		console.log(result);
+		
+		if (result === 'Correct!') {
+			return	iter(acc + 1);
+		}
+		return `Let's try again, ${name}!`;
+	}
+	return iter(0);
+};
+
+console.log(playFullGame(3));
