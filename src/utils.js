5e9f3d757f97d3e489b53983e2746ab072a2a20d
rende11 <artamonovn@gmail.com>
2017-01-08 21:13:51 +0300

    -fix codestyle

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
 // @flow
-const getRandomInt = (min :number, max :number) => Math.floor(Math.random() * (max - min)) + min;
+const getRandomInt = (min: number, max: number) => Math.floor(Math.random() * (max - min)) + min;
 const getCustomRandomInt = () => getRandomInt(0, 100);
-const numToSortedArray = (num :number) => num.toString().split('').map(n => parseInt(n, 10)).sort()
+const numToSortedArray = (num: number) => num.toString().split('').map(n => parseInt(n, 10)).sort()
   .slice();
 
-const arrayToInt = (array :array) => parseInt(array.join(''), 10);
+const arrayToInt = (array: Array<number>) => parseInt(array.join(''), 10);
 
 export { getCustomRandomInt, getRandomInt, numToSortedArray, arrayToInt };
